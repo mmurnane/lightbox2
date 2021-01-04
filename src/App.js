833faf3eca3ade45ad61df5c6c4dc57d1004d60e
Mike <michael.j.murnane@gmail.com>
2021-01-03 22:52:55 -0500

    working prototype

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
 import "./App.css";
-import styled from "styled-components";
-import { FixedSizeList as List } from "react-window";
+import styled from "@emotion/styled";
 import React, { useState, useRef, useEffect, useCallback } from "react";
-import { useTransition, animated } from "react-spring";
-import { useGesture } from "react-use-gesture";
-import clamp from "lodash-es/clamp";
-
-function useOutsideAlerter(ref, setStatus, setZoom) {
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (ref.current && event.target === ref.current) {
-        setZoom(null);
-        setStatus(false);
-      }
-    }
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [ref, setStatus]);
-}
+import Lightbox from "./Lightbox";
 
 const Button = styled.button`
   margin: 0;
@@ -50,101 +32,11 @@ const GridImage = styled.img`
   transform: translate(-50%, -50%);
 `;
 
-const Overlay = styled.div`
-  height: 100vh;
-  width: 100vw;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  background-color: rgb(0, 0, 0);
-  background-color: rgba(0, 0, 0, 0.9);
-  overflow-x: hidden;
-  transition: 0.5s;
-`;
-
-const LightboxImage = styled(animated.img)`
-  max-width: 100%;
-  max-height: 100%;
-  align-self: flex-end;
-`;
-
-const LightBoxWrapper = styled.div`
-  position: relative;
-  margin: auto;
-  height: 97vh;
-  width: 98vw;
-  border: 1px solid red;
-`;
-
-const ContentWrapper = styled(animated.div)`
-  position: absolute;
-  top: 30%; /* position the top  edge of the element at the middle of the parent */
-  left: 50%; /* position the left edge of the element at the middle of the parent */
-  transform: translate(-50%, -50%);
-  border: 1px solid purple;
-  width: 80%;
-  height: 60%;
-  display: flex;
-  text-align: center;
-  justify-content: center; /* align horizontal */
-`;
-
-const ScrollWrapper = styled.div`
-  position: absolute;
-  top: 70%; /* position the top  edge of the element at the middle of the parent */
-  left: 50%; /* position the left edge of the element at the middle of the parent */
-  transform: translate(-50%, -50%);
-  border: 1px solid purple;
-`;
-
-const PhotoScroll = styled(List)`
-  border: 1px solid #d9dddd;
-`;
-
-const Column = ({ index, style, data }) => {
-  return (
-    <button onClick={() => data.setIdx(index)}>
-      <img style={style} src={data.images[index].url} />;
-    </button>
-  );
-};
-
-const Example = ({ images, idx, photoRef, setIdx }) => {
-  useEffect(() => {
-    idx && photoRef.current.scrollToItem(idx, "center");
-  }, [idx, photoRef]);
-
-  return (
-    <PhotoScroll
-      height={75}
-      itemCount={50}
-      itemSize={100}
-      itemData={{ images, selectedIndex: idx, setIdx }}
-      layout="horizontal"
-      width={0.6 * document.documentElement.clientWidth}
-      ref={photoRef}
-    >
-      {Column}
-    </PhotoScroll>
-  );
-};
-
 function App() {
   const [status, setStatus] = useState(false);
   const [images, setImages] = useState([]);
-  const [zoom, setZoom] = useState(null);
-  const [imageMove, setImageMove] = useState({ x: 0, y: 0 });
-  const [idx, setIdx] = useState(null);
-  const photoLocateRef = useRef();
-  const wrapperRef = useRef(null);
-  const moveRef = useRef(true);
-  useOutsideAlerter(wrapperRef, setStatus, setZoom);
 
-  useEffect(() => {
-    let browserZoomLevel = Math.round(window.devicePixelRatio * 100);
-    console.log(browserZoomLevel);
-  });
+  const [idx, setIdx] = useState(null);
 
   useEffect(() => {
     function fetchImages() {
@@ -164,81 +56,14 @@ function App() {
     !status && (document.body.style.overflow = "unset");
   }, [status]);
 
-  const onClick = useCallback(() => setIdx((indexNo) => indexNo + 1), []);
-  const onClickLeft = useCallback(() => setIdx((indexNo) => indexNo - 1), []);
-
-  const bind = useGesture({
-    onDrag: ({
-      active,
-      movement: [mx],
-      direction: [xDir],
-      distance,
-      offset: [dx, dy],
-      cancel,
-    }) => {
-      if (
-        active &&
-        distance > 0 &&
-        moveRef.current &&
-        Math.round(window.devicePixelRatio * 100) <= zoom
-      ) {
-        moveRef.current = false;
-        cancel(
-          setIdx((c) => {
-            return clamp(c + (xDir > 0 ? -1 : 1), 0, images.length - 1);
-          })
-        );
-      }
-      if (active && Math.round(window.devicePixelRatio * 100) > zoom) {
-        setImageMove({ x: dx, y: dy });
-      }
-      if (!active) {
-        moveRef.current = true;
-      }
-      return;
-    },
-    onWheel: ({ wheeling, direction: [xdir, ydir] }) =>
-      wheeling &&
-      Math.round(window.devicePixelRatio * 100) <= zoom &&
-      setIdx((c) => {
-        return clamp(c + ydir, 0, images.length - 1);
-      }),
-    onPinch: (x) => console.log(x),
-  });
-
-  const bind2 = useGesture({
-    onDrag: ({
-      active,
-      movement: [mx],
-      direction: [xDir],
-      distance,
-      offset: [dx, dy],
-      cancel,
-    }) => {
-      setImageMove({ x: dx, y: dy });
-    },
-  });
-
-  const transitions = useTransition(
-    images[idx],
-    (item) => {
-      return item && item.id;
-    },
-    {
-      from: { opacity: 0 },
-      enter: { opacity: 1 },
-      leave: { opacity: 0 },
-    }
-  );
-
   return (
     <div className="App">
+      <div>HI!</div>
       <PhotoGrid>
         {images.map((x, i) => (
           <Button
             key={i}
             onClick={() => {
-              setZoom(Math.round(window.devicePixelRatio * 100));
               setIdx(i);
               setStatus((c) => !c);
             }}
@@ -249,37 +74,12 @@ function App() {
       </PhotoGrid>
 
       {status && (
-        <Overlay>
-          <LightBoxWrapper
-            style={{ left: imageMove.x, top: imageMove.y }}
-            ref={wrapperRef}
-          >
-            {transitions.map(({ item, props, key }) => {
-              return (
-                item && (
-                  <ContentWrapper {...bind()} key={key}>
-                    <LightboxImage
-                      onClick={onClick}
-                      key={key}
-                      style={props}
-                      src={item.url}
-                      alt="logo"
-                    />
-                  </ContentWrapper>
-                )
-              );
-            })}
-
-            <ScrollWrapper>
-              <Example
-                photoRef={photoLocateRef}
-                setIdx={setIdx}
-                images={images}
-                idx={idx}
-              />
-            </ScrollWrapper>
-          </LightBoxWrapper>
-        </Overlay>
+        <Lightbox
+          idx={idx}
+          setIdx={setIdx}
+          images={images}
+          setStatus={setStatus}
+        />
       )}
     </div>
   );
---
+++ src/Lightbox.js
@@ -0,0 +1,223 @@
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  useCallback,
+  useReducer,
+} from "react";
+import styled from "@emotion/styled";
+import { FixedSizeList as List } from "react-window";
+import { useTransition, animated, useSpring } from "react-spring";
+import { useGesture } from "react-use-gesture";
+import clamp from "lodash-es/clamp";
+
+const Overlay = styled.div`
+  height: 100vh;
+  width: 100vw;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.9);
+  overflow-x: hidden;
+  transition: 0.5s;
+`;
+
+const LightboxImage = styled(animated.img)`
+  align-self: flex-end;
+  max-width: 100%;
+  max-height: 100%;
+`;
+
+const LightBoxWrapper = styled.div`
+  position: relative;
+  margin: auto;
+  height: 97vh;
+  width: 98vw;
+  border: 1px solid red;
+`;
+
+const ContentWrapper = styled(animated.div)`
+  position: absolute;
+  top: 30%; /* position the top  edge of the element at the middle of the parent */
+  left: 50%; /* position the left edge of the element at the middle of the parent */
+  transform: translate(-50%, -50%);
+  border: 1px solid purple;
+  width: 80%;
+  height: 60%;
+  display: flex;
+  text-align: center;
+  justify-content: center; /* align horizontal */
+`;
+
+const ScrollWrapper = styled.div`
+  position: absolute;
+  top: 70%; /* position the top  edge of the element at the middle of the parent */
+  left: 50%; /* position the left edge of the element at the middle of the parent */
+  transform: translate(-50%, -50%);
+  border: 1px solid purple;
+`;
+
+const PhotoScroll = styled(List)`
+  border: 1px solid #d9dddd;
+`;
+
+function useOutsideAlerter(ref, setStatus, setZoom) {
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && event.target === ref.current) {
+        setZoom(1);
+        setStatus(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, setStatus, setZoom]);
+}
+
+const Column = ({ index, style, data }) => {
+  return (
+    <button onClick={() => data.setIdx(index)}>
+      <img style={style} src={data.images[index].url} />;
+    </button>
+  );
+};
+
+const Example = ({ images, idx, photoRef, setIdx }) => {
+  useEffect(() => {
+    idx && photoRef.current.scrollToItem(idx, "center");
+  }, [idx, photoRef]);
+
+  return (
+    <PhotoScroll
+      height={75}
+      itemCount={50}
+      itemSize={100}
+      itemData={{ images, selectedIndex: idx, setIdx }}
+      layout="horizontal"
+      width={0.6 * document.documentElement.clientWidth}
+      ref={photoRef}
+    >
+      {Column}
+    </PhotoScroll>
+  );
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "zoom":
+      return { ...state, scale: action.scale };
+    case "pan":
+      return { ...state, x: action.x, y: action.y };
+    case "reset":
+      return { x: 0, y: 0, scale: 1 };
+    default:
+      throw new Error();
+  }
+}
+
+function Lightbox(props) {
+  const { idx, setIdx, images, setStatus } = props;
+  const [zoom, setZoom] = useState(1);
+  const [zoomState, dispatch] = useReducer(reducer, { x: 0, y: 0, scale: 1 });
+  const photoLocateRef = useRef();
+  const wrapperRef = useRef();
+  const lightboxZoomRef = useRef();
+
+  useOutsideAlerter(wrapperRef, setStatus, setZoom);
+  const onClick = useCallback(() => setIdx((indexNo) => indexNo + 1), []);
+
+  useGesture(
+    {
+      onDrag: ({
+        active,
+        direction: [xDir],
+        distance,
+        offset: [x, y],
+        cancel,
+      }) => {
+        console.log("DRAG INITIATED: ", zoomState.scale);
+        if (active && distance > 0 && zoomState.scale === 1) {
+          cancel(
+            setIdx((c) => {
+              return clamp(c + (xDir > 0 ? -1 : 1), 0, images.length - 1);
+            })
+          );
+        }
+        if (active && zoomState.scale > 1) {
+          console.log("GETTING HERE: ", x, y);
+          dispatch({ type: "pan", x, y });
+        }
+        return;
+      },
+      onWheel: ({ wheeling, direction: [xdir, ydir] }) =>
+        wheeling &&
+        zoomState.scale === 1 &&
+        setIdx((c) => {
+          return clamp(c + ydir, 0, images.length - 1);
+        }),
+      onPinch: ({ offset: [d] }) => {
+        console.log("Pinch Detected");
+        let zoomCalc = Math.max(1 + d / 50, 1);
+        zoomCalc === 1 && dispatch({ type: "reset" });
+        zoomCalc !== 1 && dispatch({ type: "zoom", scale: zoomCalc });
+        return;
+      },
+    },
+    { domTarget: lightboxZoomRef, eventOptions: { passive: false } }
+  );
+
+  const transitions = useTransition(
+    images[idx],
+    (item) => {
+      return item && item.id;
+    },
+    {
+      from: { opacity: 0 },
+      enter: { opacity: 1 },
+      leave: { opacity: 0 },
+    }
+  );
+  return (
+    <Overlay ref={lightboxZoomRef}>
+      <LightBoxWrapper
+        ref={wrapperRef}
+        //style={{  }}
+      >
+        {transitions.map(({ item, props, key }) => {
+          return (
+            item && (
+              <ContentWrapper key={key}>
+                <LightboxImage
+                  style={{
+                    ...props,
+                    touchAction: "none",
+                    transform: `scale(${zoomState.scale}) translate(${zoomState.x}px, ${zoomState.y}px)`,
+                  }}
+                  /*  onClick={onClick} */
+                  key={key}
+                  src={item.url}
+                  alt="logo"
+                />
+              </ContentWrapper>
+            )
+          );
+        })}
+
+        <ScrollWrapper>
+          <Example
+            photoRef={photoLocateRef}
+            setIdx={setIdx}
+            images={images}
+            idx={idx}
+          />
+        </ScrollWrapper>
+      </LightBoxWrapper>
+    </Overlay>
+  );
+}
+
+export default Lightbox;
